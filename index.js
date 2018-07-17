628b02bbaf289f4ba5c00adcb6c180033cc8d23b
fantasyui-com <captain@fantasyui.com>
2018-07-16 22:43:27 -0400

    Progress Information

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ const home = require('home');
 const sha = require('sha');
 const chalk = require('chalk');
 
+
+
+
+
 const cid = new Set();
 
 program
   .version('0.1.0')
   .arguments('<dir> [dirs...]')
+
+  .option('-f, --use-fingerprinting', 'fingerprint files with sha to avoid copies.')
 
   .option('--title', 'Just use album+title in a single dir.')
   .option('--artist-title', 'Use artist/album+title in a two dir configuration')
@@ -41,132 +47,136 @@ if ( (typeof dirValues === 'undefined') || (dirValues.length === 0) ) {
    console.error('no dir[s] given!');
    process.exit(1);
 }
+console.log(dirValues);
 
-
-let readData = async function({location}){
-  return mm.parseFile(location, {native: true})
+let readData = function({location}){
+  return new Promise(function (resolve, reject){
+    mm.parseFile(location, {native: true}).then(function(data){
+      //setTimeout(function(){
+        resolve( data );
+      //}, 1000);
+    });
+  })
 };
 
-const main = async function({dir}){
 
-  //
-  let files = glob.sync( "**/*", {realpath:true, nodir:true, cwd:dir} )
-  .filter(i=>fs.statSync(i).isFile() ) // strip dot files
-  .filter(i=>!i.match(/^\./)) // strip dot files
-  .filter(i=>i.match(/\.mp3$/)) // match mp3 only
-  .map( i => ({ // create object
-    sha: ""+sha.getSync(i),
-    location:i,
-    name: kebabCase(path.basename(i, path.extname(i))) + path.extname(i)
-  }))
+scan(dirValues);
 
-  let entries = [];
+function scan (dirs){
 
-  files.forEach( file => {
+  let files = [];
+  dirs.forEach(dir => {
 
-    if( cid.has(file.sha) ){
-       // do not use this
-       // console.info('Skipped: [%s]', file.location)
-    }else{
-       entries.push(file)
-       cid.add(file.sha);
-    }
+    console.info(`Scanning source directory ${dir}...`)
+    files = files.concat(
+      glob.sync( "**/*", {realpath:true, nodir:true, cwd:dir} )
+      .filter(i=>fs.statSync(i).isFile() ) // strip dot files
+      .filter(i=>!i.match(/^\./)) // strip dot files
+      .filter(i=>i.match(/\.mp3$/)) // match mp3 only
+      .map( i => ({ // create object
+        location:i,
+        name: kebabCase(path.basename(i, path.extname(i))) + path.extname(i)
+      })))
+    console.info(`Found ${files.length} mp3 file${files.length>1?'s':''} in ${dir}`);
   });
 
+  if(program.useFingerprinting){
+  console.info(`Fingerprinting a total of ${files.length} mp3 file${files.length>1?'s':''}...`)
 
-  entries.forEach(async (entry, index) => {
+  files.forEach( (file, index )=>{
+    let number = index + 1;
+    process.stdout.write(`${number}/${files.length}`);
+    file.sha = sha.getSync(file.location);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+  });
 
-    try {
-
-
-      let data = await readData(entry);
-
-      //console.log(data.common)
-      const oldName = entry.location;
-
-      let newName = "";
-
-      //console.log(data)
-
-      let album  = data.common.album || 'Various';
-      let artist = data.common.artist || data.common.artist || albumartist || (artists||[]).join(", ") || 'Unknown';
-      let title  = data.common.title || 'Untitled Song' + ' ' + shortid.generate();
-
-      //console.log( album, artist, title, );
-
-      album = album.trim();
-      artist = artist.trim();
-      title = title.trim();
-
-
-      album = kebabCase(album);
-      artist = kebabCase(artist);
-      title = kebabCase(title);
-
-      //console.log( album, artist, title, );
-
-      const ext = path.extname(entry.location);
-
-
-      if(program.title){
-        const dirname = path.join(program.musicLibrary);
-        const filename = album + '--' + title + ext;
-        newName = path.join(dirname, filename);
-      } else if(program.artistTitle){
-        const dirname = path.join(program.musicLibrary, artist);
-        const filename = album + '--' + title + ext;
-        newName = path.join(dirname, filename);
-      } else if(program.artistAlbumTitle){
-        const dirname = path.join(program.musicLibrary, artist, album);
-        const filename = title + ext;
-        newName = path.join(dirname, filename);
+    let entries = [];
+    files.forEach( file => {
+      if( cid.has(file.sha) ){
+        console.info('Skipped: [%s]', file.location)
       }else{
-        const dirname = path.join(program.musicLibrary, artist);
-        const filename = album + '--' + title + ext;
-        newName = path.join(dirname, filename);
+         entries.push(file)
+         cid.add(file.sha);
       }
+    });
+    doit(entries);
+  }else{
+    doit(files);
+  }
 
-      //console.log( newName );
+} // scan
 
-      // if(program.albumName){
-      //   newName = path.join( dir, padStart(index+1, (files.length+"").length , '0') +'-'+ kebabCase(data.common.title||data.common.album) + path.extname(entry.location) );
-      //
-      // } else if(program.trackAlbumName){
-      //   newName = path.join( dir, padStart(data.common.track.no, (files.length+"").length , '0') +'-'+ kebabCase(data.common.title||data.common.album) + path.extname(entry.location) );
-      //
-      // } else if(program.diskTrackAlbumName){
-      //   newName = path.join( dir, padStart(data.common.disk.no, (files.length+"").length , '0') +'-'+ padStart(data.common.track.no, (files.length+"").length , '0') +'-'+ kebabCase(data.common.title||data.common.album) + path.extname(entry.location) );
-      //
-      // } else {
-      //   if(!data.common.title) throw new Error('Missing Title')
-      //   newName = path.join( dir, padStart(data.common.track.no, (files.length+"").length , '0') +'-'+ kebabCase(data.common.title||data.common.album) + path.extname(entry.location) );
-      // }
+function doit (entries){
 
-      //console.log( "%s\n%s\n\n", oldName, newName )
+    let root = Promise.resolve();
 
+  entries.forEach((entry, index) => {
 
-        if(program.dryRun){
+    root = root.then(i=>readData(entry).then(function(data){
 
-          // console.log( "%s\n%s\n\n", oldName, newName )
-          console.log( "%s -> %s", chalk.red(oldName), chalk.green(newName) )
-
-        } else if(program.moveFiles){
-        //  fs.ensureDirSync( path.dirname( newName ) );
-        //  fs.renameSync( oldName, newName );
-        }else{
-         fs.ensureDirSync( path.dirname( newName ) );
-         fs.copySync( oldName, newName );
-        }
+        let number = index + 1;
+        console.log(`Processing ${number}/${entries.length}: ${entry.location}`);
 
 
+          const oldName = entry.location;
+          let newName = "";
 
-    } catch(e){
-      console.log(e)
-    }
+          let album  = data.common.album || 'Various';
+          let artist = data.common.artist || data.common.artist || data.common.albumartist || (data.common.artists||[]).join(", ") || 'Unknown';
+          let title  = data.common.title || 'Untitled Song' + ' ' + shortid.generate();
 
-  });
+          album = album.trim();
+          artist = artist.trim();
+          title = title.trim();
 
-} // main
+          album = kebabCase(album);
+          artist = kebabCase(artist);
+          title = kebabCase(title);
+
+          if(artist === 'various') artist = album;
+
+          const ext = path.extname(entry.location);
+
+          if(program.title){
+            const dirname = path.join(program.musicLibrary);
+            const filename = album + '--' + title + ext;
+            newName = path.join(dirname, filename);
+          } else if(program.artistTitle){
+            const dirname = path.join(program.musicLibrary, artist);
+            const filename = album + '--' + title + ext;
+            newName = path.join(dirname, filename);
+          } else if(program.artistAlbumTitle){
+            const dirname = path.join(program.musicLibrary, artist, album);
+            const filename = title + ext;
+            newName = path.join(dirname, filename);
+          }else{
+            const dirname = path.join(program.musicLibrary, artist);
+            const filename = album + '--' + title + ext;
+            newName = path.join(dirname, filename);
+          }
+
+          if(program.dryRun){
+            // console.log( "%s\n%s\n\n", oldName, newName )
+             console.log( "%s -> %s", chalk.red(oldName), chalk.green(newName) )
+          } else if(program.moveFiles){
+            //  fs.ensureDirSync( path.dirname( newName ) );
+            //  fs.renameSync( oldName, newName );
+          }else{
+            fs.ensureDirSync( path.dirname( newName ) );
+            fs.copySync( oldName, newName );
+          }
 
 
-dirValues.map(dir=>main({dir}))
+
+
+    }) // read data
+  ) // root.then
+
+  }) // forEach
+
+  root.then(function(){
+    console.log('Finished...');
+  })
+
+} // doit
